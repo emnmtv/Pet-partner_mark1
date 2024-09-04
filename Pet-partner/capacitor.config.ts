35a3b30d2db610047b961fe6cb5bfb7f50b383ad
emnmtv <90402959+emnmtv@users.noreply.github.com>
2024-09-05 00:56:34 +0800

    dw
    wd

---
+++ Pet-partner/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'pet-partner',
+  webDir: 'dist/pet-partner/browser'
+};
+
+export default config;
---
+++ Pet-partner/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    HomeComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
